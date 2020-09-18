450a55dab41193fe6d79dbd7322bb387022e35ad
simpavel <nezt@ya.ru>
2020-09-18 13:13:24 +0700

    just checking things

---
+++ JUnitTests/sim/pavel/PropertiesTest.java
@@ -0,0 +1,28 @@
+package sim.pavel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertiesTest {
+
+    @Test
+    void fromArgs() {
+    }
+
+    @Test
+    void isDescendingSortOrder() {
+    }
+
+    @Test
+    void isDataTypeIsInt() {
+    }
+
+    @Test
+    void getInputFileNames() {
+    }
+
+    @Test
+    void getOutputFileName() {
+    }
+}
