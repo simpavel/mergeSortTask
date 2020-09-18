e0224182fa793b21514f69225c9f62a18cad57f1
simpavel <nezt@ya.ru>
2020-09-18 15:20:21 +0700

    getValue

--- JUnitTests/sim/pavel/PropertiesTest.java
+++ JUnitTests/sim/pavel/PropertiesTest.java
@@ -1,13 +1,32 @@
 package sim.pavel;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PropertiesTest {
+    @ParameterizedTest
+    @CsvSource({
+            "-d, -i, test\\out.txt, test\\in1.txt, test\\in3.txt",
+            "-d, -s, test\\out.txt, test\\in1.txt, test\\in3.txt",
+    })
+    void testFromArgsDescending(String a, String b, String c, String d, String e) {
+        Properties properties = Properties.fromArgs(new String[] {a, b, c, d, e});
+        assertTrue(properties.isDescendingSortOrder());
+    }
 
-    @Test
-    void fromArgs() {
+    @ParameterizedTest
+    @CsvSource({
+            "-a, -i, test\\out.txt, test\\in1.txt, test\\in3.txt",
+            "-a, -s, test\\out.txt, test\\in1.txt, test\\in3.txt",
+            "-i, test\\out.txt, test\\in1.txt, test\\in3.txt, test\\in3.txt",
+            "-s, test\\out.txt, test\\in1.txt, test\\in3.txt, test\\in3.txt",
+    })
+    void testFromArgsAscending(String a, String b, String c, String d, String e) {
+        Properties properties = Properties.fromArgs(new String[] {a, b, c, d, e});
+        assertFalse(properties.isDescendingSortOrder());
     }
 
     @Test
@@ -25,4 +44,5 @@ class PropertiesTest {
     @Test
     void getOutputFileName() {
     }
+
 }
