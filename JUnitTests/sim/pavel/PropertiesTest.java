c9a48b9ecf3201bbea79874fba22eabf4b149870
simpavel <nezt@ya.ru>
2020-09-18 16:19:12 +0700

    getValue

--- JUnitTests/sim/pavel/PropertiesTest.java
+++ JUnitTests/sim/pavel/PropertiesTest.java
@@ -30,25 +30,66 @@ class PropertiesTest {
     }
 
     @Test
-    void isDescendingSortOrder() {
+    void testIfDescendingSortOrder() {
         Properties properties = Properties.fromArgs(new String[] {"-i", "something", "something"});
         assertFalse(properties.isDescendingSortOrder());
+
         properties = Properties.fromArgs(new String[] {"-a", "something", "something"});
         assertFalse(properties.isDescendingSortOrder());
+
         properties = Properties.fromArgs(new String[] {"-d", "something", "something"});
         assertTrue(properties.isDescendingSortOrder());
     }
 
     @Test
-    void isDataTypeIsInt() {
+    void isDataTypeInt() {
+        Properties properties = Properties.fromArgs(new String[] {"-i", "something", "something"});
+        assertTrue(properties.isDataTypeInt());
+
+        properties = Properties.fromArgs(new String[] {"-s", "something", "something"});
+        assertFalse(properties.isDataTypeInt());
+
+        properties = Properties.fromArgs(new String[] {"-a", "-i", "something"});
+        assertTrue(properties.isDataTypeInt());
+
+        properties = Properties.fromArgs(new String[] {"-d", "-s", "something"});
+        assertFalse(properties.isDataTypeInt());
     }
 
-    @Test
-    void getInputFileNames() {
-    }
+    @ParameterizedTest
+    @CsvSource({
+            "-a, -i, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-a, -s, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-d, -i, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-d, -s, test\\out.txt, test\\in1.txt, test\\in2.txt",
+    })
+    void getInputFileNamesWithFirstArgument(String a, String b, String c, String d, String e) {
+        Properties properties = Properties.fromArgs(new String[] {a, b, c, d, e});
+        assertEquals(2, properties.getInputFileNames().size());
+        }
 
-    @Test
-    void getOutputFileName() {
+    @ParameterizedTest
+    @CsvSource({
+            "-i, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-s, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-i, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-s, test\\out.txt, test\\in1.txt, test\\in2.txt",
+    })
+    void getInputFileNamesWithoutFirstArgument(String a, String b, String c, String d) {
+        Properties properties = Properties.fromArgs(new String[] {a, b, c, d});
+        assertEquals(2, properties.getInputFileNames().size());
+        }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-a, -i, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-a, -s, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-d, -i, test\\out.txt, test\\in1.txt, test\\in2.txt",
+            "-d, -s, test\\out.txt, test\\in1.txt, test\\in2.txt",
+    })
+    void getOutputFileName(String a, String b, String c, String d, String e) {
+        Properties properties = Properties.fromArgs(new String[] {a, b, c, d, e});
+        assertEquals("test\\out.txt", properties.getOutputFileName());
     }
 
 }
